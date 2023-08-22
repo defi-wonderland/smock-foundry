0e94d7c2c7f619ef997f02b37b4d38d6be394998
Dristpunk <dristpunk@gmail.com>
2023-08-22 13:34:34 +0300

    fix: export internal func template

--- copy-templates.js
+++ copy-templates.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+// prettier-ignore
+
 /**
  * Copies the mockContractTemplate.hbs file from the src/templates folder to the dist/templates folder
  */
@@ -10,18 +12,21 @@ const copyTemplate = () => {
   const mockArrayTemplatePath = path.resolve(__dirname, 'src', 'templates', 'mockArrayStateVariableTemplate.hbs');
   const mockBasicStateVarTemplatePath = path.resolve(__dirname, 'src', 'templates', 'mockBasicStateVariableTemplate.hbs');
   const mockExternalFunctionTemplatePath = path.resolve(__dirname, 'src', 'templates', 'mockExternalFunctionTemplate.hbs');
+  const mockInternalFunctionTemplatePath = path.resolve(__dirname, 'src', 'templates', 'mockInternalFunctionTemplate.hbs');
   const mockMappingTemplatePath = path.resolve(__dirname, 'src', 'templates', 'mockMappingStateVariableTemplate.hbs');
 
   const destinationPath = path.resolve(__dirname, 'dist/templates', 'mockContractTemplate.hbs');
   const destinationArrayPath = path.resolve(__dirname, 'dist/templates', 'mockArrayStateVariableTemplate.hbs');
   const destinationBasicStateVarPath = path.resolve(__dirname, 'dist/templates', 'mockBasicStateVariableTemplate.hbs');
   const destinationExternalFunctionPath = path.resolve(__dirname, 'dist/templates', 'mockExternalFunctionTemplate.hbs');
+  const destinationInternalFunctionPath = path.resolve(__dirname, 'dist/templates', 'mockInternalFunctionTemplate.hbs');
   const destinationMappingPath = path.resolve(__dirname, 'dist/templates', 'mockMappingStateVariableTemplate.hbs');
 
   fs.copyFileSync(templatePath, destinationPath);
   fs.copyFileSync(mockArrayTemplatePath, destinationArrayPath);
   fs.copyFileSync(mockBasicStateVarTemplatePath, destinationBasicStateVarPath);
   fs.copyFileSync(mockExternalFunctionTemplatePath, destinationExternalFunctionPath);
+  fs.copyFileSync(mockInternalFunctionTemplatePath, destinationInternalFunctionPath);
   fs.copyFileSync(mockMappingTemplatePath, destinationMappingPath);
 };
 
