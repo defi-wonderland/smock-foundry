abba1221c6ad7a743d653dca17a4b49506ab0bad
dristpunk <107591874+dristpunk@users.noreply.github.com>
2023-11-22 18:25:03 +0500

    refactor: cp entire template dir (#13)

--- copy-templates.js
+++ copy-templates.js
@@ -2,48 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 /**
- * Copies the *.hbs files from the src/templates folder to the dist/templates folder
+ * Copies the src/templates folder to the dist/templates folder
  */
 const copyTemplates = () => {
-  fs.mkdirSync(path.resolve(__dirname, 'dist/templates'));
-  const templatePath = path.resolve(__dirname, 'src', 'templates', 'mockContractTemplate.hbs');
-  const mockArrayTemplatePath = path.resolve(__dirname, 'src', 'templates', 'mockArrayStateVariableTemplate.hbs');
-  const mockBasicStateVarTemplatePath = path.resolve(
-    __dirname,
-    'src',
-    'templates',
-    'mockBasicStateVariableTemplate.hbs'
-  );
-  const mockExternalFunctionTemplatePath = path.resolve(
-    __dirname,
-    'src',
-    'templates',
-    'mockExternalFunctionTemplate.hbs'
-  );
-  const mockInternalFunctionTemplatePath = path.resolve(
-    __dirname,
-    'src',
-    'templates',
-    'mockInternalFunctionTemplate.hbs'
-  );
-  const mockMappingTemplatePath = path.resolve(__dirname, 'src', 'templates', 'mockMappingStateVariableTemplate.hbs');
-  const mockHelperTemplatePath = path.resolve(__dirname, 'src', 'templates', 'mockHelperTemplate.hbs');
-
-  const destinationPath = path.resolve(__dirname, 'dist/templates', 'mockContractTemplate.hbs');
-  const destinationArrayPath = path.resolve(__dirname, 'dist/templates', 'mockArrayStateVariableTemplate.hbs');
-  const destinationBasicStateVarPath = path.resolve(__dirname, 'dist/templates', 'mockBasicStateVariableTemplate.hbs');
-  const destinationExternalFunctionPath = path.resolve(__dirname, 'dist/templates', 'mockExternalFunctionTemplate.hbs');
-  const destinationInternalFunctionPath = path.resolve(__dirname, 'dist/templates', 'mockInternalFunctionTemplate.hbs');
-  const destinationMappingPath = path.resolve(__dirname, 'dist/templates', 'mockMappingStateVariableTemplate.hbs');
-  const destinationHelperPath = path.resolve(__dirname, 'dist/templates', 'mockHelperTemplate.hbs');
-
-  fs.copyFileSync(templatePath, destinationPath);
-  fs.copyFileSync(mockArrayTemplatePath, destinationArrayPath);
-  fs.copyFileSync(mockBasicStateVarTemplatePath, destinationBasicStateVarPath);
-  fs.copyFileSync(mockExternalFunctionTemplatePath, destinationExternalFunctionPath);
-  fs.copyFileSync(mockInternalFunctionTemplatePath, destinationInternalFunctionPath);
-  fs.copyFileSync(mockMappingTemplatePath, destinationMappingPath);
-  fs.copyFileSync(mockHelperTemplatePath, destinationHelperPath);
+  try {
+    fs.cpSync('src/templates', 'dist/templates', { recursive: true, overwrite: true });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 copyTemplates();
