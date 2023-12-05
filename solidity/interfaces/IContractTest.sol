b0da78f2f9a2bfb2bca822dfadd6cd8e17ed25d3
0xJabberwock <102967621+0xJabberwock@users.noreply.github.com>
2023-12-05 18:18:10 -0300

    feat: mock array mappings (#25)

--- solidity/interfaces/IContractTest.sol
+++ solidity/interfaces/IContractTest.sol
@@ -32,8 +32,9 @@ interface IContractTest {
   function uint256ToAddress(uint256) external view returns (address);
   function addressToUint(address) external view returns (uint256);
   function bytes32ToBytes(bytes32) external view returns (bytes memory);
-  function uint256ToAddressToBytes32(uint256, address) external view returns (bytes32);
   function uint256ToMyStruct(uint256) external view returns (uint256, string memory);
+  function uint256ToAddressArray(uint256, uint256) external view returns (address);
+  function uint256ToAddressToBytes32(uint256, address) external view returns (bytes32);
   function immutableUintVariable() external view returns (uint256);
   // Logic
   function setVariables(
